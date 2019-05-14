48dc50b0a6340b74487097a0cf590c6f3df3cab3
Paolo Gallo <s223793@studenti.polito.it>
2019-05-14 22:05:28 +0200

    Added Camera and GameObject3D documentation, moved those classes in the /Engine subfolder and namespace

--- oldgoldmine-game/Engine/Camera.cs
+++ oldgoldmine-game/Engine/Camera.cs
@@ -2,8 +2,9 @@
 using Microsoft.Xna.Framework;
 
 
-namespace oldgoldmine_game
+namespace oldgoldmine_game.Engine
 {
+
     public class GameCamera
     {
         private Vector3 target;
@@ -22,15 +23,17 @@ namespace oldgoldmine_game
 
         public Vector3 Position { get { return position; } private set { } }
         public Vector3 Target { get { return target; } set { LookAt(value); } }
-        
+
+
+        // Normalized vectors representing the 6 directions relative to the camera's coordinate system (viewpoint)
         public Vector3 Forward { get { return Vector3.Normalize(direction); } }
         public Vector3 Back { get { return Vector3.Normalize(-direction); } }
         public Vector3 Left { get { return Vector3.Normalize(new Vector3(direction.Z, 0f, -direction.X)); } }
         public Vector3 Right { get { return Vector3.Normalize(new Vector3(-direction.Z, 0f, direction.X)); } }
-        public Vector3 Up { get { return Vector3.Normalize(new Vector3(direction.X, direction.Z, -direction.Y)); } }
-        public Vector3 Down { get { return Vector3.Normalize(new Vector3(-direction.X, -direction.Z, direction.Y)); } }
+        public Vector3 Up { get { return Vector3.Normalize(new Vector3(direction.X, direction.Z, -direction.Y)); } }        // TODO: Up and Down vectors are
+        public Vector3 Down { get { return Vector3.Normalize(new Vector3(-direction.X, -direction.Z, direction.Y)); } }     // wrong when rotating the camera
 
-        
+
 
 
         /// <summary>
@@ -63,8 +66,9 @@ namespace oldgoldmine_game
         }
 
         /// <summary>
-        /// Point the camera to look at the specified position in tri-dimensional space
+        /// Point the camera to look at the specified position.
         /// </summary>
+        /// <param name="targetPosition">The coordinates to look at.</param>
         public void LookAt(Vector3 targetPosition)
         {
             this.target = targetPosition;
@@ -73,8 +77,10 @@ namespace oldgoldmine_game
 
         /// <summary>
         /// Move the camera in an arbitrary direction in tri-dimensional space, 
-        /// it will keep looking in the same direction
+        /// it will keep looking in the same direction.
         /// </summary>
+        /// <param name="speed">The movement speed.</param>
+        /// <param name="direction">A normalized Vector3 representing the direction of the movement.</param>
         public void Move(float speed, Vector3 direction)
         {
             this.position += speed * direction;
@@ -83,8 +89,10 @@ namespace oldgoldmine_game
 
         /// <summary>
         /// Move the camera in an arbitrary direction in tri-dimensional space, 
-        /// it will keep looking at the previous target and therefore rotate the view
+        /// the view will be locked on the previous target and therefore the camera will rotate.
         /// </summary>
+        /// <param name="speed">The movement speed.</param>
+        /// <param name="direction">A normalized Vector3 representing the direction of the movement.</param>
         public void MoveKeepTarget(float speed, Vector3 direction)
         {
             this.position += speed * direction;
@@ -92,8 +100,11 @@ namespace oldgoldmine_game
         }
 
 
-
-        public void RotateViewVertical(float degrees)
+        /// <summary>
+        /// Rotate (vertically) the direction in which the camera is looking.
+        /// </summary>
+        /// <param name="degrees">The amount of rotation, > 0 to look upwards, < 0 to look downwards.</param>
+        public void RotateViewVertical(float degrees)               // TODO: limit the vertical camera rotation between minLookDownAngle and maxLookUpAngle
         {
             Matrix t = Matrix.CreateTranslation(-position);
             Quaternion q = Quaternion.CreateFromAxisAngle(this.Right, MathHelper.ToRadians(degrees));
@@ -102,6 +113,10 @@ namespace oldgoldmine_game
         }
 
 
+        /// <summary>
+        /// Rotate (horizontally) the direction in which the camera is looking.
+        /// </summary>
+        /// <param name="degrees">The amount of degrees of the rotation, > 0 to rotate Right, < 0 to rotate Left.</param>
         public void RotateViewHorizontal(float degrees)
         {
             Matrix t = Matrix.CreateTranslation(-position);
---
+++ oldgoldmine-game/Engine/GameObject3D.cs
@@ -0,0 +1,176 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+
+namespace oldgoldmine_game.Engine
+{
+
+    class GameObject3D
+    {
+        private readonly Model model3d;
+
+        private Vector3 position;
+        private Vector3 scale;
+        private Quaternion rotation;
+
+
+        public Vector3 Position { get { return position; } set { position = value; } }
+        public Vector3 Scale { get { return scale; } set { scale = value; } }
+        public Quaternion Rotation { get { return rotation; } set { rotation = value; } }
+
+        private Matrix objectWorldMatrix;
+        private bool updated = false;
+
+        /// <summary>
+        /// Get the world matrix representing the coordinate system of this object, incorporating any position,
+        /// rotation and scale transformation that has been applied to it. Use this for rendering the 3D model.
+        /// </summary>
+        public Matrix ObjectWorldMatrix { get { if (!updated) this.Update(); return objectWorldMatrix; } }
+
+
+        /// <summary>
+        /// Construct an empty GameObject3D, with default position, rotation and size.
+        /// </summary>
+        public GameObject3D()
+        {
+            this.model3d = null;
+            this.position = Vector3.Zero;
+            this.scale = Vector3.One;
+            this.rotation = Quaternion.Identity;
+        }
+
+        /// <summary>
+        /// Construct a GameObject3D around an imported Model, with default position, rotation and size.
+        /// </summary>
+        /// <param name="model">The 3D model of this object.</param>
+        public GameObject3D(Model model)
+        {
+            this.model3d = model;
+            this.position = Vector3.Zero;
+            this.scale = Vector3.One;
+            this.rotation = Quaternion.Identity;
+        }
+
+        /// <summary>
+        /// Construct a GameObject3D around an imported Model, initializing its position, rotation and size.
+        /// </summary>
+        /// <param name="model">The 3D model of this object.</param>
+        /// <param name="position">The initial position of the object.</param>
+        /// <param name="scale">The scale factor applied to the model's size.</param>
+        /// <param name="rotation">The initial rotation of the 3D model.</param>
+        public GameObject3D(Model model, Vector3 position, Vector3 scale, Quaternion rotation)
+        {
+            this.model3d = model;
+            this.position = position;
+            this.scale = scale;
+            this.rotation = rotation;
+        }
+
+
+        /// <summary>
+        /// Change the position of this object in 3D coordinate space.
+        /// </summary>
+        /// <param name="movement">A Vector3 representing the amount of movement to apply on each axis.</param>
+        public void MovePosition(Vector3 movement)
+        {
+            position += movement;
+            updated = false;
+        }
+
+        /// <summary>
+        /// Change the scale (size) of the 3D model.
+        /// </summary>
+        /// <param name="scale">A value representing the uniform scaling factor for the entire object.</param>
+        public void ScaleSize(float scale)
+        {
+            this.scale.X = scale;
+            this.scale.Y = scale;
+            this.scale.Z = scale;
+            updated = false;
+        }
+
+        /// <summary>
+        /// Change the scale (size) of the 3D model.
+        /// </summary>
+        /// <param name="scale">A Vector3 representing the scaling factors for each axis.</param>
+        public void ScaleSize(Vector3 scale)
+        {
+            this.scale = scale;
+            updated = false;
+        }
+
+        /// <summary>
+        /// Rotate the 3D model around a custom axis in tridimensional space.
+        /// </summary>
+        /// <param name="axis">A Vector3 representing the axis to rotate around.</param>
+        /// <param name="degrees">The value of the rotation amount (in degrees).</param>
+        public void RotateAroundAxis(Vector3 axis, float degrees)
+        {
+            rotation *= Quaternion.CreateFromAxisAngle(axis, MathHelper.ToRadians(degrees));        // TODO: rotation changing the coordinate system
+            updated = false;                                                                        // of the object in a weird way
+        }
+
+
+        /// <summary>
+        /// Change the transparency of the object by setting the alpha value of all its meshes.
+        /// </summary>
+        /// <param name="alpha">The new alpha value for the mesh.</param>
+        public void SetAlpha(float alpha)
+        {
+            if (model3d == null)
+                return;
+
+            foreach (ModelMesh mesh in model3d.Meshes)
+            {
+                foreach (BasicEffect effect in mesh.Effects)
+                {
+                    effect.Alpha = alpha;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Enables the basic XNA lighting model for all meshes of the object
+        /// </summary>
+        public void EnableLightingModel()
+        {
+            if (model3d == null)
+                return;
+
+            foreach (var mesh in model3d.Meshes)
+            {
+                foreach (BasicEffect effect in mesh.Effects)
+                {
+                    effect.EnableDefaultLighting();
+                    effect.PreferPerPixelLighting = true;
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// Apply all previous changes to the object's Position, Rotation and Scale, updating the ObjectWorldMatrix.
+        /// </summary>
+        public void Update()
+        {
+            // Update objectWorldMatrix
+            objectWorldMatrix = Matrix.CreateWorld(Vector3.Zero, Vector3.Forward, Vector3.Up)
+                * Matrix.CreateScale(scale)
+                * Matrix.CreateFromQuaternion(rotation)
+                * Matrix.CreateTranslation(position);
+
+            updated = true;
+        }
+
+
+        /// <summary>
+        /// Render the object in 3D space, with the previously defined Position, Rotation and Scale.
+        /// </summary>
+        /// <param name="camera">The camera that will be used to render the object.</param>
+        public void Draw(in GameCamera camera)
+        {
+            model3d.Draw(this.ObjectWorldMatrix, camera.View, camera.Projection);
+        }
+
+    }
+}
