d893abe909147f16330490944b98a53d06666bd2
Paolo Gallo <s257428@studenti.polito.it>
2021-03-04 17:44:30 +0100

    Update project to MonoGame 3.8

--- oldgoldmine-game/Program.cs
+++ oldgoldmine-game/Program.cs
@@ -2,21 +2,13 @@
 
 namespace oldgoldmine_game
 {
-#if WINDOWS || LINUX
-    /// <summary>
-    /// The main class.
-    /// </summary>
     public static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
-            using (var game = new OldGoldMineGame())
-                game.Run();
+            using var game = new OldGoldMineGame();
+            game.Run();
         }
     }
-#endif
 }
